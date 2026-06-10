49f54dea8ec05228fdb01bdf4709276f841a0742
Egor Malyshkin <malyshkin@hightech.group>
2026-06-10 20:56:08 +0300

    Move ruleset seed identifiers

--- server/src/GenesysForge.Infrastructure/Persistence/Seed/RulesetSeedData.cs
+++ server/src/GenesysForge.Infrastructure/Persistence/Seed/RulesetSeedData.cs
@@ -1,21 +1,10 @@
 using GenesysForge.Domain.Rules;
+using static GenesysForge.Infrastructure.Persistence.Seed.RulesetSeedIds;
 
 namespace GenesysForge.Infrastructure.Persistence.Seed;
 
 internal static class RulesetSeedData
 {
-    public static readonly Guid DemoRulesetId = Guid.Parse("10000000-0000-0000-0000-000000000001");
-    public static readonly Guid DemoSourceBookId = Guid.Parse("10000000-0000-0000-0000-000000000002");
-    public static readonly Guid DemoSourceVersionId = Guid.Parse("10000000-0000-0000-0000-000000000003");
-    public static readonly Guid GuardianArchetypeId = Guid.Parse("10000000-0000-0000-0000-000000000004");
-    public static readonly Guid MysticArchetypeId = Guid.Parse("10000000-0000-0000-0000-000000000005");
-    public static readonly Guid ResolveSkillId = Guid.Parse("10000000-0000-0000-0000-000000000006");
-    public static readonly Guid TacticsSkillId = Guid.Parse("10000000-0000-0000-0000-000000000007");
-    public static readonly Guid GuardianDefinitionId = Guid.Parse("10000000-0000-0000-0000-000000000008");
-    public static readonly Guid MysticDefinitionId = Guid.Parse("10000000-0000-0000-0000-000000000009");
-    public static readonly Guid ResolveDefinitionId = Guid.Parse("10000000-0000-0000-0000-00000000000a");
-    public static readonly Guid TacticsDefinitionId = Guid.Parse("10000000-0000-0000-0000-00000000000b");
-
     public static Ruleset Ruleset => Ruleset.Create(
         "Демо-набор Genesys Forge",
         "1.0",
